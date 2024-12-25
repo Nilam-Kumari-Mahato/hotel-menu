080675d239d2db28ecf989a93a874efe64e312dd
Nilam Kumari Mahato <nilamkumarimahato10@gmail.com>
2024-12-25 07:37:54 +0530

    remove feature added

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let added=document.getElementById("addtolist");
 
 
 function addtolist() {
+    added.textContent="";
     for(let i=0;i<list.length;i++){
         added.innerHTML+=`<li>${list[i]}</li>`;
     }
@@ -66,4 +67,94 @@ function addnankhatai() {
 function addjalebi() {
     count10++;
     list[9]=`${count10} Jalebi`;
+}
+
+
+function remove1() {
+    count1--;
+    if(count1==0){
+        list[0]=``;
+    }
+    else{
+        list[0]=`${count1} South Indian Thali`;
+    }
+}
+function remove2() {
+    count2--;
+    if(count2==0){
+        list[1]=``;
+    }
+    list[1]=`${count2} Sahi Paneer`
+}
+function remove3() {
+    count3--;
+    if(count3==0){
+        list[2]=``;
+    }
+    else{
+        list[2]=`${count3} Samosa`;
+    }
+}
+function remove4() {
+    count4--;
+    if(count4==0){
+        list[3]=``;
+    }
+    else{
+    list[3]=`${count4} Chole Bhature`;
+    }
+}
+function remove5() {
+    count5--;
+    if(count5==0){
+        list[4]=``;
+    }
+    else{
+        list[4]=`${count5} Biryani`;
+    }
+}
+function remove6() {
+    count6--;
+    if(count6==0){
+        list[5]=``;
+    }
+    else{
+        list[5]=`${count6} Butter Chicken`;
+    }
+}
+function remove7() {
+    count7--;
+    if(count7==0){
+        list[6]=``;
+    }
+    else{
+        list[6]=`${count7} Kulcha Roti`;
+    }
+}
+function remove8() {
+    count8--;
+    if(count8==0){
+        list[7]=``;
+    }
+    else{
+        list[7]=`${count8} Kalakand`;
+    }
+}
+function remove9() {
+    count9--;
+    if(count9==0){
+        list[8]=``;
+    }
+    else{
+        list[8]=`${count9} Nankhatai`
+    }
+}
+function remove10() {
+    count10--;
+    if(count10==0){
+        list[9]=``;
+    }
+    else{
+        list[9]=`${count10} Jalebi`;
+    }
 }
